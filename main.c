54ae43ecf233d1a3986cc87f24e42e7a19c97be2
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-01-13 14:10:57 +0100

    Implemented basic cat funcitonality

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
 
 static const char *program;
@@ -12,8 +14,47 @@ const char *next_arg(int *argc, const char ***argv) {
 	return arg;
 }
 
+int read_file(const char *path, char **content, size_t *len) {
+	errno = 0;
+
+	FILE *fp = fopen(path, "rb");
+	if(fp == NULL) goto error;
+
+	if(fseek(fp, 0, SEEK_END)) goto error;
+
+	*len = ftell(fp);
+	if(*len == -1) goto error;
+
+	if(fseek(fp, 0, SEEK_SET)) goto error;
+
+	*content = (char*) malloc(*len);
+	if(*content == NULL) goto error;
+
+	if(fread(*content, sizeof(char), *len, fp) != *len) goto error;
+	
+	fclose(fp);
+
+	return 0;
+error:
+	if(fp) fclose(fp);
+	return 1;
+}
+
+void print_file(const char *path) {
+	char *content;
+	size_t len;
+
+	if(read_file(path, &content, &len)) {
+		fprintf(stderr, "%s: %s: %s\n", program, path, strerror(errno));
+	} else {
+		printf("%*s\n", (int) len, content);
+	}
+}
+
 int main(int argc, const char **argv) {
 	program = next_arg(&argc, &argv);
-
-	printf("%s\n", program);
+	
+	while(argc) {
+		print_file(next_arg(&argc, &argv));
+	}
 }
