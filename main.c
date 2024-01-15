5ae433e0c1079b0e9714512428bf5d8918e37c86
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-01-15 11:54:13 +0100

    Moved main.c/read_file to read_file.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <stdlib.h>
 
+#include "read_file.h"
 #include "formatter.h"
 
 static const char *program;
@@ -14,32 +14,6 @@ const char *next_arg(int *argc, const char ***argv) {
 	--*argc;
 	++*argv;
 	return arg;
-}
-
-int read_file(const char *path, char **content, size_t *len) {
-	errno = 0;
-
-	FILE *fp = fopen(path, "rb");
-	if(fp == NULL) goto error;
-
-	if(fseek(fp, 0, SEEK_END)) goto error;
-
-	*len = ftell(fp);
-	if(*len == -1) goto error;
-
-	if(fseek(fp, 0, SEEK_SET)) goto error;
-
-	*content = (char*) malloc(*len);
-	if(*content == NULL) goto error;
-
-	if(fread(*content, sizeof(char), *len, fp) != *len) goto error;
-	
-	fclose(fp);
-
-	return 0;
-error:
-	if(fp) fclose(fp);
-	return 1;
 }
 
 void print_file(Lang language, const char *path) {
@@ -55,9 +29,7 @@ void print_file(Lang language, const char *path) {
 	free(content);
 }
 
-int streq(const char *a, const char *b) {
-	return strcmp(a, b) == 0;
-}
+int streq(const char *a, const char *b) { return strcmp(a, b) == 0; }
 
 int main(int argc, const char **argv) {
 	program = next_arg(&argc, &argv);
